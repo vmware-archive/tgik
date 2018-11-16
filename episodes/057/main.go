4c76ba087308b4c693ead2b326c868ff971dfa46
Kris Nova <kris@nivenly.com>
2018-11-16 14:51:02 -0800

    Update main.go

--- episodes/057/main.go
+++ episodes/057/main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
-	"github.com/hashicorp/vault/api"
-	"github.com/kubicorn/kubicorn/pkg/logger"
-	"os"
 	"fmt"
 	"io/ioutil"
+	"os"
+
+	"github.com/hashicorp/vault/api"
+	"github.com/kubicorn/kubicorn/pkg/logger"
 )
 
 func main() {
 
-
-
 	config := &api.Config{
-		Address: "https://127.0.0.1:8200",
+		Address: "https://tgik:8200",
 	}
 	client, err := api.NewClient(config)
 	if err != nil {
 		logger.Critical("Unable to create client: %v", err)
 		os.Exit(1)
 	}
-
 
 	// ---------------------------------------------------
 	// Kubernetes Auth Method
@@ -33,40 +31,37 @@ func main() {
 		logger.Critical("Unable to read service account: %v", err)
 		os.Exit(1)
 	}
-	//options := map[string]interface{}{
-	//	"role": "vault-operator-role",
-	//  "jwt": string(bytes),
-	//}
-	//path := "auth/kubernetes/login"
-	//secret, err := client.Logical().Write(path, options)
-	//if err != nil {
-	//	logger.Critical("Unable to login with Kubernetes auth method: %v", err)
-	//	os.Exit(1)
-	//}
-	//token, err := secret.TokenID()
-	//if err != nil {
-	//	logger.Critical("Unable to get token with Kubernetes auth method: %v", err)
-	//	os.Exit(1)
-	//}
+	options := map[string]interface{}{
+		"role": "demo",
+		"jwt":  string(bytes),
+	}
+	path := "auth/kubernetes/login"
+	secret, err := client.Logical().Write(path, options)
+	if err != nil {
+		logger.Critical("Unable to login with Kubernetes auth method: %v", err)
+		os.Exit(1)
+	}
+	token, err := secret.TokenID()
+	if err != nil {
+		logger.Critical("Unable to get token with Kubernetes auth method: %v", err)
+		os.Exit(1)
+	}
 	//
 	// ---------------------------------------------------
-
-
 
 	// ---------------------------------------------------
 	// Token
 	//
 	//
 	// Define the token here to authenticate with Vault
-	token := ""
+	//token := ""
 	client.SetToken(token)
 	//
 	// ---------------------------------------------------
 
-
 	client.Logical().Write("/secret/tgik-secret", map[string]interface{}{
 		"value": "tgik is awesome",
-		"foo": "bar",
+		"foo":   "bar",
 	})
 
 	secretValues, err := client.Logical().Read("/secret/tgik-secret")
@@ -75,7 +70,5 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("%+v\n", secretValues)
-
-	client.Auth().Token().
 
 }
